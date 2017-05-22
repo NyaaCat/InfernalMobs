e9f47a471a0a45bd83702e926aaf5fcc58f79ff2
Recursive G <g.recursive@gmail.com>
2017-05-22 11:03:40 -0500

    Revert "name based dedup"
    This reverts commit 4ae973a531e32d1d3d1bfac9e0ad9314ef5dbc63.

--- src/main/java/com/jacob_vejvoda/infernal_mobs/LootManager.java
+++ src/main/java/com/jacob_vejvoda/infernal_mobs/LootManager.java
@@ -247,7 +247,7 @@ public class LootManager {
         LootConfig cfg = new LootConfig();
         cfg.dropMap = new HashMap<>();
         cfg.lootItems = new HashMap<>();
-        Map<String, String> itemNameMap = new HashMap<>(); // Map<itemDisplayName, index>
+        Map<ItemStack, String> reverseLookupMap = new HashMap<>();
         YamlConfiguration sec = YamlConfiguration.loadConfiguration(f);
         for (String itemIdx : sec.getConfigurationSection("loot").getKeys(false)) {
             ConfigurationSection s = sec.getConfigurationSection("loot." + itemIdx);
@@ -260,24 +260,15 @@ public class LootManager {
                 minLevel = maxLevel;
                 maxLevel = tmp;
             }
-            if (!l.item.getItemMeta().hasDisplayName()) { //not dedup on item without name
+            if (!reverseLookupMap.containsKey(l.item)) {
                 for (Integer lv = minLevel; lv <= maxLevel; lv++) {
                     cfg.setDropChance(lv, itemIdx, chance);
                 }
                 cfg.lootItems.put(itemIdx, l);
-            } else {
-                String name = ChatColor.stripColor(l.item.getItemMeta().getDisplayName());
-                if (!itemNameMap.containsKey(name)) { // first time see an item
-                    for (Integer lv = minLevel; lv <= maxLevel; lv++) {
-                        cfg.setDropChance(lv, itemIdx, chance);
-                    }
-                    cfg.lootItems.put(itemIdx, l);
-                    itemNameMap.put(name, itemIdx);
-                } else {
-                    String idx = itemNameMap.get(name);
-                    for (Integer lv = minLevel; lv <= maxLevel; lv++) {
-                        cfg.addDropChance(lv, idx, chance);
-                    }
+                reverseLookupMap.put(l.item, itemIdx);
+            } else { // duplicated item?
+                for (Integer lv = minLevel; lv <= maxLevel; lv++) {
+                    cfg.addDropChance(lv, reverseLookupMap.get(l.item), chance);
                 }
             }
         }
