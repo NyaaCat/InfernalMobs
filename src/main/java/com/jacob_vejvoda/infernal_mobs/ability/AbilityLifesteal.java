6fe2fce2365ff47069aec8674b23cdd11cb62ff0
ReinWD <ReinWDD@gmail.com>
2017-12-18 19:33:33 +0800

    fix bug of AbilityLifeSteal(use actual damage instead of real damage)

--- src/main/java/com/jacob_vejvoda/infernal_mobs/ability/AbilityLifesteal.java
+++ src/main/java/com/jacob_vejvoda/infernal_mobs/ability/AbilityLifesteal.java
@@ -9,7 +9,7 @@ import org.bukkit.event.entity.EntityDamageByEntityEvent;
 public class AbilityLifesteal implements IAbility {
     @Override
     public void onAttackPlayer(LivingEntity mobEntity, Mob mob, Player victim, boolean isDirectAttack, EntityDamageByEntityEvent ev) {
-        double damage = ev.getDamage();
+        double damage = ev.getFinalDamage();
         double newHealth = mobEntity.getHealth() + damage;
         double maxHealth = mobEntity.getAttribute(Attribute.GENERIC_MAX_HEALTH).getValue();
         if (newHealth > maxHealth) newHealth = maxHealth;
