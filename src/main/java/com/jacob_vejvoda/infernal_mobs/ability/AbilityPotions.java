3b4d50287a5c9ab047eec92d2dac09920fe8b126
cyilin <cyilin@users.noreply.github.com>
2017-12-21 21:19:40 +0800

    add potion color

--- src/main/java/com/jacob_vejvoda/infernal_mobs/ability/AbilityPotions.java
+++ src/main/java/com/jacob_vejvoda/infernal_mobs/ability/AbilityPotions.java
@@ -2,30 +2,28 @@ package com.jacob_vejvoda.infernal_mobs.ability;
 
 import com.jacob_vejvoda.infernal_mobs.Helper;
 import com.jacob_vejvoda.infernal_mobs.persist.Mob;
+import org.bukkit.Color;
 import org.bukkit.Material;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.ThrownPotion;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.PotionMeta;
-import org.bukkit.potion.PotionData;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
-import org.bukkit.potion.PotionType;
 import org.bukkit.util.Vector;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class AbilityPotions implements IAbility {
-    private static final List<PotionEffect> POTION_EFFECTS = new ArrayList<PotionEffect>(){{
-        add(new PotionEffect(PotionEffectType.HARM, 1, 2));
-        add(new PotionEffect(PotionEffectType.HARM, 1, 1));
-        add(new PotionEffect(PotionEffectType.POISON, 100, 1));
-        add(new PotionEffect(PotionEffectType.SLOW, 100, 1));
-        add(new PotionEffect(PotionEffectType.WEAKNESS, 100, 1));
+    private static final List<PotionEffect> POTION_EFFECTS = new ArrayList<PotionEffect>() {{
+        add(new PotionEffect(PotionEffectType.HARM, 1, 2, false, true, Color.fromRGB(4393481)));
+        add(new PotionEffect(PotionEffectType.HARM, 1, 1, false, true, Color.fromRGB(4393481)));
+        add(new PotionEffect(PotionEffectType.POISON, 100, 1, false, true, Color.fromRGB(5149489)));
+        add(new PotionEffect(PotionEffectType.SLOW, 100, 1, false, true, Color.fromRGB(5926017)));
+        add(new PotionEffect(PotionEffectType.WEAKNESS, 100, 1, false, true, Color.fromRGB(4738376)));
     }};
 
     @Override
@@ -38,7 +36,9 @@ public class AbilityPotions implements IAbility {
 
         ItemStack item = new ItemStack(Material.SPLASH_POTION);
         PotionMeta pm = (PotionMeta) item.getItemMeta();
-        pm.addCustomEffect(Helper.randomItem(POTION_EFFECTS), false);
+        PotionEffect effect = Helper.randomItem(POTION_EFFECTS);
+        pm.addCustomEffect(effect, false);
+        pm.setColor(effect.getColor());
         item.setItemMeta(pm);
 
         t.setItem(item);
