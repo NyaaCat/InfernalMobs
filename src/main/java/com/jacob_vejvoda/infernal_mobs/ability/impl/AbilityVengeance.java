ab1408c5159098bf3a3565796e59b5f299e3873b
ReinWD <ReinWDD@gmail.com>
2019-05-31 17:23:42 +0800

    tweak Vengeance.

--- src/main/java/com/jacob_vejvoda/infernal_mobs/ability/impl/AbilityVengeance.java
+++ src/main/java/com/jacob_vejvoda/infernal_mobs/ability/impl/AbilityVengeance.java
@@ -7,10 +7,15 @@ import com.jacob_vejvoda.infernal_mobs.persist.Mob;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
 
 public class AbilityVengeance implements IAbility {
     @Override
     public void onPlayerAttack(LivingEntity mobEntity, Mob mob, Player attacker, boolean isDirectAttack, EntityDamageByEntityEvent ev) {
-        if (Helper.possibility(0.5)) attacker.damage(ConfigReader.getVengeanceDamage() * 2);
+        if (ev.getCause().equals(EntityDamageEvent.DamageCause.THORNS))return;
+        if (Helper.possibility(0.5)) {
+//            attacker.damage(ConfigReader.getVengeanceDamage() * 2);
+            attacker.damage(ev.getDamage() * (((double) ConfigReader.getVengeanceDamage()) / 100d));
+        }
     }
 }
